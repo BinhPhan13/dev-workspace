de63d1f337dd1d744baf6bbead556af67251754d
Binh Phan <ptbinh2003@gmail.com>
2024-09-03 16:31:37 +0700

    update rye install steps

--- tools/rye.sh
+++ tools/rye.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+which rye && exit 1
+
 curl "https://rye.astral.sh/get" |
 RYE_VERSION='0.39.0' RYE_INSTALL_OPTION='--yes' \
-RYE_TOOLCHAIN_VERSION='cpython@3.11.9' bash
+RYE_TOOLCHAIN_VERSION='cpython@3.11.9' bash || exit 1
 
+cmd='source $HOME/.rye/env'
+grep -q "$cmd" $HOME/.bashrc ||
 echo "# RYE
-source $HOME/.rye/env
+$cmd
 " >> $HOME/.bashrc
 
-source $HOME/.rye/env
-mkdir -p ~/.local/share/bash-completion/completions
-rye self completion > ~/.local/share/bash-completion/completions/rye.bash
+eval $cmd
+mkdir -p $HOME/.local/share/bash-completion/completions
+rye self completion > $HOME/.local/share/bash-completion/completions/rye.bash
 
