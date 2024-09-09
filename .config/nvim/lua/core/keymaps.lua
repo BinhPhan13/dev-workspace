d0cc2f621a1f12c90a87c79a4c4b665a7d2d3b29
Binh Phan <ptbinh2003@gmail.com>
2024-09-10 00:12:07 +0700

    add some keymaps, enable signcolumn

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,12 +1,21 @@
 vim.g.mapleader = " "
 
+-- misc
 vim.keymap.set("n", "<Esc>", vim.cmd.nohls)
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "J", "J^")
 vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<leader><Tab>", "<C-6>")
+vim.keymap.set("n", "<leader>c", ":lc")
+
+-- replace word under cursor globally
 vim.keymap.set("n", "<leader>s", ":%s/<C-r><C-w>/<C-r><C-w>/g<Left><Left>")
+
+-- paste and select the pasted text
 vim.keymap.set("n", "<C-p>", "m'gpkV''j")
 
+-- void register
 vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("x", "<leader>d", "\"_d")
 
@@ -25,4 +34,10 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+
+-- tab management
+vim.keymap.set("n", "<leader>to", vim.cmd.tabnew)
+vim.keymap.set("n", "<leader>tx", vim.cmd.tabclose)
+vim.keymap.set("n", "<leader>tp", function() vim.cmd("silent! -tabm") end)
+vim.keymap.set("n", "<leader>tn", function() vim.cmd("silent! +tabm") end)
 
