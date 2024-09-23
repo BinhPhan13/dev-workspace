47dcbdb206089893c388a9a373783bb14d5103ea
Binh Phan <ptbinh2003@gmail.com>
2024-09-23 11:38:49 +0700

    add bufferline.nvim plugin, rework some keymaps for tab and buffer

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -14,10 +14,6 @@ vim.keymap.set("n", "<leader>C", function()
   vim.cmd.cd(dirpath)
 end)
 
--- wipe out buffers
-vim.keymap.set("n", "<leader>bd", vim.cmd.bw)
-vim.keymap.set("n", "<leader>bD", function() vim.cmd("%bw") end)
-
 -- terminal mode
 vim.keymap.set("n", "<leader>T", vim.cmd.term)
 vim.keymap.set("t", "<leader>\\", "<C-\\><C-n>")
@@ -39,7 +35,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
 
--- window management
+-- window
 vim.keymap.set("n", "<leader>wv", "<C-w>v")
 vim.keymap.set("n", "<leader>wh", "<C-w>s")
 vim.keymap.set("n", "<leader>wx", "<C-w>c")
@@ -49,12 +45,20 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
 
--- tab management
+-- tab
 vim.keymap.set("n", "<leader>to", function()
   bufpath = vim.api.nvim_buf_get_name(0)
-  vim.cmd.tabe(bufpath)
+  pcall(vim.cmd.tabe, bufpath)
 end)
-vim.keymap.set("n", "<leader>tx", vim.cmd.tabclose)
-vim.keymap.set("n", "<leader>tp", function() vim.cmd("silent! -tabm") end)
-vim.keymap.set("n", "<leader>tn", function() vim.cmd("silent! +tabm") end)
+vim.keymap.set("n", "<leader>tx", function() pcall(vim.cmd.tabc) end)
+
+vim.keymap.set("n", "<M-h>", function() pcall(vim.cmd, "-tabm") end)
+vim.keymap.set("n", "<M-l>", function() pcall(vim.cmd, "+tabm") end)
+
+-- buffer
+vim.keymap.set("n", "<leader>bd", function()
+  ok, _ = pcall(vim.cmd, "bn | bw #")
+  if not ok then pcall(vim.cmd, "enew | bw #") end
+end)
+vim.keymap.set("n", "<leader>bD", function() vim.cmd("%bw") end)
 
---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,29 @@
+local function config()
+  local options = {
+    mode = "tabs",
+    numbers = "ordinal",
+
+    tab_size = 17,
+    max_name_length = 17,
+    show_tab_indicators = false,
+
+    show_buffer_close_icons = false,
+    show_duplicate_prefix = false,
+
+    close_command = nil,
+    right_mouse_command = nil,
+    left_mouse_command = nil,
+    middle_mouse_command = nil,
+
+    name_formatter = function(buf)
+      return vim.fn.fnamemodify(buf.path, ":~:.")
+    end
+  }
+  require("bufferline").setup({ options = options })
+end
+
+return {
+  "akinsho/bufferline.nvim",
+  config = config
+}
+
