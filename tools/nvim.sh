16098287871227bed27d4ff99090272dca4be8a6
Binh Phan <ptbinh2003@gmail.com>
2025-05-18 16:29:49 +0700

    update setup script for nvim and tmux

--- tools/nvim.sh
+++ tools/nvim.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-if [[ -d $HOME/.local/nvim-linux64/ ]]; then echo "existed !" ; exit 1; fi
+if ! [ -d $HOME/.local/nvim-linux64 ]; then
+  url="https://github.com/neovim/neovim/releases/download/
+      v0.10.3/nvim-linux64.tar.gz"
+  curl -L ${url//[[:space:]]/} -o hic || exit 1
 
-url="https://github.com/neovim/neovim/releases/download/
-    v0.10.3/nvim-linux64.tar.gz"
-curl -L ${url//[[:space:]]/} -o hic || exit 1
-
-mkdir -p $HOME/.local/
-tar xf hic -C $HOME/.local/
-rm hic
+  mkdir -p $HOME/.local/
+  tar xf hic -C $HOME/.local/ && rm hic
+fi
 
 mkdir -p $HOME/.local/bin/
-ln -sf $HOME/.local/nvim-linux64/bin/nvim $HOME/.local/bin/vi
+ln -sf $HOME/.local/nvim-linux64/bin/nvim $HOME/.local/bin/nv
+
+rm -f $HOME/.local/bin/vi &&
+echo '#!/bin/bash
+nv --noplugin $@
+' > $HOME/.local/bin/vi &&
+chmod +x $HOME/.local/bin/vi
 
--- tools/tmux/setup.sh
+++ tools/tmux/setup.sh
@@ -7,13 +7,7 @@ cp $dir/tmux $HOME/.local/bin/
 mkdir -p $HOME/.local/share/man/man1/
 cp $dir/tmux.1 $HOME/.local/share/man/man1/
 
-url="https://raw.githubusercontent.com/imomaliev/
-    tmux-bash-completion/master/completions/tmux"
-curl -L ${url//[[:space:]]/} -o tmux.bash || exit 1
-
-mkdir -p $HOME/.local/share/bash-completion/completions/
-mv tmux.bash $HOME/.local/share/bash-completion/completions/
-
-rm -rf $HOME/.local/tmux-plugins/
+if ! [ -d $HOME/.local/tmux-plugins/ ]; then
 git clone https://github.com/tmux-plugins/tpm $HOME/.local/tmux-plugins/
+fi
 
